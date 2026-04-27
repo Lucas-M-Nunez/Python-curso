8150f4f087788014b066c454e93cdb0f2fe3db08
Lucas M. Nuñez <114788609+Lucas-M-Nunez@users.noreply.github.com>
2026-04-26 21:46:35 -0300

    actividad1

---
+++ Ejercicios/Actividad1-LucasNuñez.py
@@ -0,0 +1,12 @@
+# Actividad 1: Promedio Ponderado
+# nota_1 = float(input("Ingrese la primera nota: "))
+# nota_2 = float(input("Ingrese la segunda nota: "))
+# promedio = (nota_1 * 0.4) + (nota_2 * 0.6)
+# print("El promedio ponderado es:", promedio)
+
+#Slicing
+
+nombre_completo = 'Lucas Martin Nuñez'
+nombre_completo[:5]
+
+print(nombre_completo)
